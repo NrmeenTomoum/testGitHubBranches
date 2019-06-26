1e6bbcc8e2aedf0803bbc8fe1aa2adbc8e7a05c0
Nermeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2019-06-26 10:17:13 +0400

    Update ViewController.swift

--- testGitHubBranches/ViewController.swift
+++ testGitHubBranches/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// remote changes
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
